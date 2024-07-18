502e1f48f934c887a78356f4b8c36c9b04e79ef7
mickaelrei <mickael.reichert@gmail.com>
2024-07-17 23:54:02 -0300

    Changed logic for testing scenes

--- tests/main.cpp
+++ tests/main.cpp
@@ -22,6 +22,8 @@ private:
     std::shared_ptr<tmig::Scene> lightsScene;
     std::shared_ptr<tmig::Entity> movingEntity;
     std::shared_ptr<tmig::Entity> rotatingEntity;
+
+    bool flashlightFollowing = true;
 };
 
 void App::setup() {
@@ -47,41 +49,43 @@ void App::setup() {
 
     // Create entities to scene
     {
+        float d = 10.0f;
         auto wallLeft = std::make_shared<Entity>(utils::boxGMesh());
-        wallLeft->setScale(glm::vec3{1.0f, 10.0f, 10.0f});
-        wallLeft->setPosition(glm::vec3{-5.0f, 0.0f, 0.0f});
+        wallLeft->setScale(glm::vec3{1.0f, d * 2.0f, d * 2.0f});
+        wallLeft->setPosition(glm::vec3{-d, 0.0f, 0.0f});
         // wallLeft->setColor(glm::vec4{1.0f, 0.0f, 0.0f, 1.0f});
 
         auto wallRight = std::make_shared<Entity>(utils::boxGMesh());
-        wallRight->setScale(glm::vec3{1.0f, 10.0f, 10.0f});
-        wallRight->setPosition(glm::vec3{5.0f, 0.0f, 0.0f});
+        wallRight->setScale(glm::vec3{1.0f, d * 2.0f, d * 2.0f});
+        wallRight->setPosition(glm::vec3{d, 0.0f, 0.0f});
         // wallRight->setColor(glm::vec4{0.0f, 1.0f, 0.0f, 1.0f});
         wallRight->setRotation(glm::rotate(glm::mat4{1.0f}, glm::radians(180.f), glm::vec3{0.0f, 0.0f, 1.0f}));
 
         auto wallBack = std::make_shared<Entity>(utils::boxGMesh());
-        wallBack->setScale(glm::vec3{10.0f, 10.0f, 1.0f});
-        wallBack->setPosition(glm::vec3{0.0f, 0.0f, 5.0f});
+        wallBack->setScale(glm::vec3{d * 2.0f, d * 2.0f, 1.0f});
+        wallBack->setPosition(glm::vec3{0.0f, 0.0f, d});
         // wallBack->setColor(glm::vec4{0.0f, 0.0f, 1.0f, 1.0f});
 
         auto wallFront = std::make_shared<Entity>(utils::boxGMesh());
-        wallFront->setScale(glm::vec3{10.0f, 10.0f, 1.0f});
-        wallFront->setPosition(glm::vec3{0.0f, 0.0f, -5.0f});
+        wallFront->setScale(glm::vec3{d * 2.0f, d * 2.0f, 1.0f});
+        wallFront->setPosition(glm::vec3{0.0f, 0.0f, -d});
         // wallFront->setColor(glm::vec4{1.0f, 1.0f, 0.0f, 1.0f});
 
         auto wallBottom = std::make_shared<Entity>(utils::boxGMesh());
-        wallBottom->setScale(glm::vec3{10.0f, 1.0f, 10.0f});
-        wallBottom->setPosition(glm::vec3{0.0f, -5.0f, 0.0f});
+        wallBottom->setScale(glm::vec3{d * 2.0f, 1.0f, d * 2.0f});
+        wallBottom->setPosition(glm::vec3{0.0f, -d, 0.0f});
         // wallBottom->setColor(glm::vec4{0.0f, 1.0f, 1.0f, 1.0f});
 
         auto wallTop = std::make_shared<Entity>(utils::boxGMesh());
-        wallTop->setScale(glm::vec3{10.0f, 1.0f, 10.0f});
-        wallTop->setPosition(glm::vec3{0.0f, 5.0f, 0.0f});
+        wallTop->setScale(glm::vec3{d * 2.0f, 1.0f, d * 2.0f});
+        wallTop->setPosition(glm::vec3{0.0f, d, 0.0f});
         // wallTop->setColor(glm::vec4{1.0f, 0.0f, 1.0f, 1.0f});
 
         movingEntity = std::make_shared<Entity>(utils::sphereGMesh());
         movingEntity->setScale(glm::vec3{0.5f});
 
         rotatingEntity = std::make_shared<Entity>(utils::torusGMesh());
+        // rotatingEntity->setScale(glm::vec3{1.0f, 3.0f, 1.0f});
         rotatingEntity->setScale(glm::vec3{0.5f});
 
         lightsScene->addEntity(wallLeft);
@@ -93,12 +97,13 @@ void App::setup() {
         lightsScene->addEntity(movingEntity);
         lightsScene->addEntity(rotatingEntity);
 
-        auto light1 = std::make_shared<PointLight>(glm::vec3{1.0f, 0.0f, 0.0f}, 0.5f, glm::vec3{0.0f, 2.0f, 0.0f});
-        auto light2 = std::make_shared<PointLight>(glm::vec3{0.0f, 1.0f, 0.0f}, 0.5f, glm::vec3{0.0f, -2.0f, 0.0f});
-        auto light3 = std::make_shared<PointLight>(glm::vec3{0.0f, 0.0f, 1.0f}, 0.5f, glm::vec3{2.0f, 0.0f, 0.0f});
-        auto light4 = std::make_shared<PointLight>(glm::vec3{1.0f, 1.0f, 0.0f}, 0.5f, glm::vec3{-2.0f, 0.0f, 0.0f});
-        auto light5 = std::make_shared<PointLight>(glm::vec3{1.0f, 0.0f, 1.0f}, 0.5f, glm::vec3{0.0f, 0.0f, 2.0f});
-        auto light6 = std::make_shared<PointLight>(glm::vec3{0.0f, 1.0f, 1.0f}, 0.5f, glm::vec3{0.0f, 0.0f, -2.0f});
+        float s = 0.75f;
+        auto light1 = std::make_shared<PointLight>(glm::vec3{1.0f, 0.0f, 0.0f}, s, glm::vec3{0.0f, 2.0f, 0.0f});
+        auto light2 = std::make_shared<PointLight>(glm::vec3{0.0f, 1.0f, 0.0f}, s, glm::vec3{0.0f, -2.0f, 0.0f});
+        auto light3 = std::make_shared<PointLight>(glm::vec3{0.0f, 0.0f, 1.0f}, s, glm::vec3{2.0f, 0.0f, 0.0f});
+        auto light4 = std::make_shared<PointLight>(glm::vec3{1.0f, 1.0f, 0.0f}, s, glm::vec3{-2.0f, 0.0f, 0.0f});
+        auto light5 = std::make_shared<PointLight>(glm::vec3{1.0f, 0.0f, 1.0f}, s, glm::vec3{0.0f, 0.0f, 2.0f});
+        auto light6 = std::make_shared<PointLight>(glm::vec3{0.0f, 1.0f, 1.0f}, s, glm::vec3{0.0f, 0.0f, -2.0f});
 
         lightsScene->addLight(light1);
         lightsScene->addLight(light2);
@@ -106,6 +111,43 @@ void App::setup() {
         lightsScene->addLight(light4);
         lightsScene->addLight(light5);
         lightsScene->addLight(light6);
+
+        auto lightCube1 = std::make_shared<Entity>(utils::boxGMesh());
+        lightCube1->setColor(glm::vec4{light1->color, 1.0f});
+        lightCube1->setPosition(glm::vec3{0.0f, 2.0f, 0.0f});
+        lightCube1->setScale(glm::vec3{.2f});
+
+        auto lightCube2 = std::make_shared<Entity>(utils::boxGMesh());
+        lightCube2->setColor(glm::vec4{light2->color, 1.0f});
+        lightCube2->setPosition(glm::vec3{0.0f, -2.0f, 0.0f});
+        lightCube2->setScale(glm::vec3{.2f});
+
+        auto lightCube3 = std::make_shared<Entity>(utils::boxGMesh());
+        lightCube3->setColor(glm::vec4{light3->color, 1.0f});
+        lightCube3->setPosition(glm::vec3{2.0f, 0.0f, 0.0f});
+        lightCube3->setScale(glm::vec3{.2f});
+
+        auto lightCube4 = std::make_shared<Entity>(utils::boxGMesh());
+        lightCube4->setColor(glm::vec4{light4->color, 1.0f});
+        lightCube4->setPosition(glm::vec3{-2.0f, 0.0f, 0.0f});
+        lightCube4->setScale(glm::vec3{.2f});
+
+        auto lightCube5 = std::make_shared<Entity>(utils::boxGMesh());
+        lightCube5->setColor(glm::vec4{light5->color, 1.0f});
+        lightCube5->setPosition(glm::vec3{0.0f, 0.0f, 2.0f});
+        lightCube5->setScale(glm::vec3{.2f});
+
+        auto lightCube6 = std::make_shared<Entity>(utils::boxGMesh());
+        lightCube6->setColor(glm::vec4{light6->color, 1.0f});
+        lightCube6->setPosition(glm::vec3{0.0f, 0.0f, -2.0f});
+        lightCube6->setScale(glm::vec3{.2f});
+
+        lightsScene->addEntity(lightCube1);
+        lightsScene->addEntity(lightCube2);
+        lightsScene->addEntity(lightCube3);
+        lightsScene->addEntity(lightCube4);
+        lightsScene->addEntity(lightCube5);
+        lightsScene->addEntity(lightCube6);
     }
 
     // ----------------------------------------------------------
@@ -125,6 +167,12 @@ void App::setup() {
         glm::radians(17.5f)
     );
     flashlightScene->addLight(flashlight);
+
+    auto floor = std::make_shared<Entity>(utils::boxGMesh());
+    floor->setColor(glm::vec4{1.0f, 0.5f, 0.25f, 1.0f});
+    floor->setScale(glm::vec3{10.f, 1.0f, 30.0f});
+    floor->setPosition(glm::vec3{0.0f, -2.0f, 0.0f});
+    flashlightScene->addEntity(floor);
 
     glm::vec3 pos[] = {
         {0.7f, 0.6f, -1.0f},
@@ -170,10 +218,10 @@ void App::update(float dt) {
         float s = 2.0f;
         movingEntity->setPosition(glm::vec3{
             std::cos(t) * s,
-            0.0f,
+            std::cos(t * 4.0f),
             std::sin(t) * s,
         });
-    } else {
+    } else if (flashlightFollowing) {
         flashlight->pos = currentScene->camera.pos;
         flashlight->dir = currentScene->camera.getForward();
     }
@@ -191,6 +239,11 @@ void App::processInput(float dt) {
         } else {
             currentScene = flashlightScene;
         }
+    }
+    if (glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS) {
+        flashlightFollowing = false;
+    } else {
+        flashlightFollowing = true;
     }
 }
 
